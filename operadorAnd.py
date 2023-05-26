aaf2a25b99e7e6545290eb910f6791a336b2b9bd
NahuelIroz <82222142+NahuelIroz@users.noreply.github.com>
2023-05-26 12:13:42 -0300

    Add files via upload

---
+++ operadorAnd.py
@@ -0,0 +1,5 @@
+numero=int(input("Ingrese un número entre 1 y 10: "))
+if(numero>=1)and(numero<=10):
+    print(numero, "Está entre 1 y 10")
+else:
+    print(numero, "está fuera de rango")
---
+++ suma.py
@@ -0,0 +1,5 @@
+numUno=int(input("Escribe el primer número: ")) #Con el int le digo que el valoir va a ser un entero
+numDos=int(input("Escribe el segundo número: "))
+suma= numUno + numDos
+print( "El resultado es:", suma)
+print("Fin del programa. ¡¡¡¡¡¡¡SALUDOS!!!!")
